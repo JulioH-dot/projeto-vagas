046ff434fef503cedab51f694962d53ee71e278a
Julio-Hdot <juliohenrique1909@gmail.com>
2022-12-05 12:30:05 -0300

    criando e estilizando o Componente Home

--- src/components/layouts/ConteudoComponent.vue
+++ src/components/layouts/ConteudoComponent.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
         <h1>Component Conteúdo</h1>
-        <Home/>
-        <PublicarVaga/>
+        <button @click="conteudo = 'home'">Home</button>
+        <button @click="conteudo = 'publicar-vaga'">Publicar Vaga</button>
+        <keep-alive>
+            <component :is="conteudo" />
+        </keep-alive>
     </div>
 </template>
 
@@ -11,14 +14,17 @@
 <script>
 
 import Home from '../views/Home.vue';
-import Vaga from '../views/Vaga.vue'
+import PublicarVaga from '../views/PublicarVaga.vue'
 
 
 export default {
     name: 'Conteudo',
+    data: () => ({
+        conteudo: 'home'
+    }),
     components:{
         Home,
-        PublicarVaga: Vaga,
+        PublicarVaga,
     }
 }
 
